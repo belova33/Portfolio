d37e1c9998c5a1e8f6b30ffe42bc41d8ab6fa0d6
belova33 <belova33@mail.ru>
2022-04-17 02:03:16 +0300

    change

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './styles/main.scss';
 import './index.html';
 
+
+newFunction();
+
+function newFunction() {
+    function openModal() {
+        document.getElementById("modal").style.top = "0px";
+    }
+    function closeModal() {
+        document.getElementById("modal").style.top = "-700px";
+    }
+}
